21d44a2d726b7c1abbce5f73477ad95819300c0e
huzaifa tahir <h905923@gmail.com>
2026-07-07 23:31:45 +0500

    Implement UserCard component layout and update admin page structure

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full h-full flex'>
-      <div className='w-full lg:w-2/3 bg-red-500'>sd</div>
+      <div className='w-full lg:w-2/3 bg-red-500'>
+      
+      </div>
       <div className='w-full lg:w-1/3 bg-blue-500'></div>
     </div>
   )
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const UserCard = ({type}: {type:string}) => {
   return (
-    <div>UserCard</div>
+    <div>
+        <div className='flex justify-between'>
+           <span>24/6/2026</span>
+           <Image src="/more.png" alt="more"  height={30} width={30}/>
+        </div>
+        <span>1024</span>
+        <span>{type}</span>
+    </div>
   )
 }
 

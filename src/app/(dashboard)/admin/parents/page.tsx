47baed466bbc761b29feca4384ade5e9466b08ef
huzaifa tahir <h905923@gmail.com>
2026-07-28 10:13:59 +0500

    feat: integrate AddRecord component for parent creation with toggle functionality

--- src/app/(dashboard)/admin/parents/page.tsx
+++ src/app/(dashboard)/admin/parents/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import TextFeild from '@/components/TextFeild'
 import Image from 'next/image'
-import React from 'react'
 import Table from '@/components/Table'
 import { parentsData } from '@/lib/data'
+import AddRecord from "@/components/AddRecord"
+import {useStore} from "@/store/useStore";
+import {Plus} from "lucide-react"
 const page = () => {
+  const toggleForm = useStore((state)=>state.toggleForm);
   const coloumn = [
     {
       accessor:"id",
@@ -41,7 +45,7 @@ const page = () => {
 ]
   return (
     <div className="px-6 md:px-10  py-4 w-full  h-full flex flex-col ">
-     
+     <AddRecord role="parent" type="create"/>
     {/* top section */}
     <div className='w-full flex gap-2 justify-between max-sm:justify-center items-center '>
         <h1 className='text-xl max-sm:text-sm'>All Parent </h1>
@@ -61,7 +65,7 @@ const page = () => {
         <div className='flex w-8 h-8  bg-lama-yellow rounded-full justify-center items-center'>
 
         
-        <Image src="/plus.png" width={20} height={5} alt="Add"  />
+        <Plus size={30} className='hover:bg-accent-300 rounded-full transition-all duration-150 o  ' onClick={()=>{toggleForm()}}   />
         
         </div>
         

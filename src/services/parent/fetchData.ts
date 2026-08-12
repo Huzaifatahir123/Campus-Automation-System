9878d16d04a94eba8195f0ca103ed721d4629d22
huzaifa tahir <h905923@gmail.com>
2026-08-12 22:13:08 +0500

    feat: update color-accent-900 in globals.css and modify getParents function to accept search parameter

--- src/services/parent/fetchData.ts
+++ src/services/parent/fetchData.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
-export async function getParents():Promise<{}>{
+export async function getParents(search: string):Promise<{}>{
              try {
-                const response = await axios.get("/api/parent/fetch");
+                const response = await axios.get(`/api/parent/fetch?search=${search}`);
                 console.log(response.data);
                 return response
              } catch (error:any) {
